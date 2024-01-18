d6b77c110f6dd1019573c61ce6bb0b69bdf4b7d2
ankita-akhadkar <156774898+ankita-akhadkar@users.noreply.github.com>
2024-01-18 18:16:13 +0530

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -1,11 +1,8 @@
 package com.ankita.TheKiranAcademy;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.query.sqm.tree.expression.SqmExpression;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,60 +20,16 @@ public class LoginController {
 	}
 
 	@RequestMapping("userlogin")
-	public String adminlogin(String username,String password) {
+	public void String adminlogin(String username,String password) {
 //		Object login;
 		if(username.equals("Ankita")&& password.equals("1234")) {
-		return"home"; 
-		
-	}	return "userlogin";
+		return "nextpage";
+	}
 	}
 	
 
-	@RequestMapping("createaccount")
-	public String createaccount() {
-		return "createaccount";
-
-	}
-	@RequestMapping("createaccountdata")//API
-	public   <Login, login> String createaccountdata(login  Login) {
-Session s=sf.openSession();
-Transaction t=s.beginTransaction();
-s.save(Login);//username/password
-t.commit();
-	return "createaccount";//page
-
 	}
 
-
-
-	@RequestMapping("homepage")
-	public String homepage() {
-		return "home";
-	}
-
-	@RequestMapping("aboutpage")
-	public String aboutpage() {
-		return "about";
-	}
-
-	@RequestMapping("servicepage")
-	public String servicepage() {
-		return "service";
-	}
-
-	@RequestMapping("contactpage")
-	public String contactpage() {
-		return "contact";
-	}
-	@RequestMapping("contactdata")//API
-	public  <contact> String contactdata(contact Contact) {
-Session s=sf.openSession();
-Transaction t=s.beginTransaction();
-s.save(Contact);//username/password
-t.commit();
-	return "contact";//page
-
-	}
 
 
 }
